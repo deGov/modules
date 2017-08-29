bffa301d0c0e86c4dc7d6e6d5e6e952bf94d6f1f
Markus Orenstrat <markus.orenstrat@reinblau.de>
2017-08-30 01:17:36 +0200

    Move logic to a service.

--- degov_multilingual/src/Controller/FrontPageController.php
+++ degov_multilingual/src/Controller/FrontPageController.php
@@ -2,9 +2,9 @@
 
 namespace Drupal\degov_multilingual\Controller;
 
-use Drupal\Core\Cache\Cache;
-use Drupal\Core\Controller\ControllerBase;
-use Drupal\node\NodeInterface;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\degov_multilingual\DegovMultilingualFrontPage;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -13,7 +13,30 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
  *
  * @package Drupal\degov_multilingual\Controller
  */
-class FrontPageController extends ControllerBase {
+class FrontPageController implements ContainerInjectionInterface {
+
+  /**
+   * @var \Drupal\degov_multilingual\DegovMultilingualFrontPage
+   */
+  protected $degovMultilingualFrontPage;
+
+  /**
+   * FrontPageController constructor.
+   *
+   * @param \Drupal\degov_multilingual\DegovMultilingualFrontPage $degovMultilingualFrontPage
+   */
+  public function __construct(DegovMultilingualFrontPage $degovMultilingualFrontPage) {
+    $this->degovMultilingualFrontPage = $degovMultilingualFrontPage;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('degov_multilingual.front_page')
+    );
+  }
 
   /**
    * Renders a node for the front_page route.
@@ -21,38 +44,19 @@ class FrontPageController extends ControllerBase {
    * @return array
    */
   public function render() {
-    $front_pages = $this->config('degov_multilingual.settings')->get('front_pages');
-    $language = $this->languageManager()->getCurrentLanguage()->getId();
-    $default_language = $this->languageManager()->getDefaultLanguage()->getId();
+    $build = $this->degovMultilingualFrontPage->getBuild();
 
-    // Try to find a node to display.
-    if (isset($front_pages[$language])) {
-      $nid = $front_pages[$language];
-    }
-    elseif (isset($front_pages[$default_language])) {
-      $nid = $front_pages[$default_language];
-    }
+    switch ($build) {
+      case DegovMultilingualFrontPage::NOT_FOUND:
+        throw new NotFoundHttpException();
+        break;
 
-    if (!empty($nid)) {
-      $node = $this->entityTypeManager()->getStorage('node')->load($nid);
-    }
-
-    // If we have a node, check access and either throw an access denied
-    // exception if access is not allowed or build and return a render array
-    // if access was allowed. If we don't have a node, return a not found
-    // exception.
-    if (!empty($node) && $node instanceof NodeInterface) {
-      if ($node->access('view')) {
-        $build = $this->entityTypeManager()->getViewBuilder('node')->view($node);
-        $build['#cache']['tags'][] = 'degov_multilingual_front_page';
-        return $build;
-      }
-      else {
+      case DegovMultilingualFrontPage::ACCESS_DENIED:
         throw new AccessDeniedHttpException();
-      }
-    }
-    else {
-      throw new NotFoundHttpException();
+        break;
+
+      default:
+        return $build;
     }
   }
 
