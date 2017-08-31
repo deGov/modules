4865628621aa2dc76a620ddf3d6925f14109cb3b
Stefan Weber <s.weber@1xinternet.de>
2017-08-31 20:14:02 +0200

    added fix for process.js

--- degov_social_media_settings/js/process.js
+++ degov_social_media_settings/js/process.js
@@ -49,7 +49,7 @@
   // Shows the social media settings link if cookie are allowed and
   // creates a cookie with default values.
   function initializeSettings() {
-    if (Drupal.eu_cookie_compliance.hasAgreed()) {
+    if (Drupal.eu_cookie_compliance !== undefined && Drupal.eu_cookie_compliance.hasAgreed()) {
       $('.js-social-media-settings-open').removeClass('hidden');
 
       if (cookieExists()) {
