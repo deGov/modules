badf483c1642102f4cd842e44ce4672595d88e76
Artem Dmitriiev <a.dmitriiev@1xinternet.de>
2017-09-06 18:10:20 +0200

    Fix template override for themes, previously theme templates from themes/nrw/ folder were not taken into account

--- degov_common/src/Common.php
+++ degov_common/src/Common.php
@@ -63,9 +63,9 @@ class Common {
     }
 
     if ($add_suggestion) {
-      $template_path = substr($info['theme path'], 0, 14);
+      $template_path = $info['theme path'];
       // Only override templates that are defined by contrib modules.
-      if ($template_path == 'themes/contrib' || $template_path != 'themes/custom/') {
+      if (strpos($template_path,'themes/') !== 0) {
         $info['theme path'] = $module_path = drupal_get_path('module', $module_name);
         $info['path'] = $module_path . '/templates';
         // Add a template for every defined view mode else add it for the default view mode.
