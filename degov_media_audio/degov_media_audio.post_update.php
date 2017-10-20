5ae99b0670c494c99ff0dc3f3980138f2f9650d0
Nicky Vandevoorde <n.vandevoorde@1xinternet.de>
2017-10-20 17:27:38 +0200

    Fix post update by disabling and enabling search indexing

--- degov_media_audio/degov_media_audio.post_update.php
+++ degov_media_audio/degov_media_audio.post_update.php
@@ -6,12 +6,21 @@
 function degov_media_audio_post_update_migrate_field_title(&$sandbox) {
   // Initialize some variables during the first pass through.
   if (!isset($sandbox['total'])) {
+    $sandbox['is_index'] = FALSE;
     $max = \Drupal::entityQuery('media')
       ->condition('bundle', 'audio')
       ->count()
       ->execute();
     $sandbox['total'] = $max;
     $sandbox['current'] = 0;
+    if (\Drupal::moduleHandler()->moduleExists('degov_search_media')) {
+      $index = \Drupal\search_api\Entity\Index::load('search_media');
+      if ($index) {
+        $index->setOption('index_directly', FALSE);
+        $index->save();
+        $sandbox['is_index'] = TRUE;
+      }
+    }
   }
 
   $media_per_batch = 50;
@@ -30,5 +39,10 @@ function degov_media_audio_post_update_migrate_field_title(&$sandbox) {
   }
 
   $sandbox['#finished'] = ($sandbox['current'] / $sandbox['total']);
+  if ($sandbox['#finished'] == 1 && $sandbox['is_index']) {
+    $index = \Drupal\search_api\Entity\Index::load('search_media');
+    $index->setOption('index_directly', TRUE);
+    $index->save();
+  }
   return t($sandbox['current'] . ' media processed.');
 }
