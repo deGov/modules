f83cffbd036c80bb56fb5ba4f3226933e083c811
Artem Dmitriiev <a.dmitriiev@1xinternet.de>
2017-10-24 15:29:26 +0200

    Transfer views reference override to degov_paragraph_view_reference

--- degov_paragraph_view_reference/src/Plugin/Field/FieldType/ViewsReferenceOverride.php
+++ degov_paragraph_view_reference/src/Plugin/Field/FieldType/ViewsReferenceOverride.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Drupal\degov_common\Plugin\Field\FieldType;
+namespace Drupal\degov_paragraph_view_reference\Plugin\Field\FieldType;
 
 use Drupal\viewsreference\Plugin\Field\FieldType\ViewsReferenceItem;
 
 /**
  * Class ViewsReferenceOverride.
  *
- * @package Drupal\degov_common\Plugin\Field\FieldType
+ * @package Drupal\degov_paragraph_view_reference\Plugin\Field\FieldType
  */
 class ViewsReferenceOverride extends ViewsReferenceItem {
 
