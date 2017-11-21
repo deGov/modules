b9d252d8a1e87d95f2949295c4a4f3254275256f
Markus Orenstrat <markus@dunix-data.de>
2017-11-21 18:51:35 +0100

    Adds a link to the media overview page.

---
+++ degov_users_roles/src/Plugin/Menu/WorkbenchMenuLinks.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\degov_users_roles\Plugin\Menu;
+
+use Drupal\Core\Menu\MenuLinkDefault;
+
+/**
+ * Class WorkbenchMenuLinks
+ *
+ * @package Drupal\degov_users_roles\Plugin\Menu
+ */
+class WorkbenchMenuLinks extends MenuLinkDefault {}
